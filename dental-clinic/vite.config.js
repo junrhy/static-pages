427270e79fe66ea91f30b18babdfec9806f0a5d0
Jun Rhy Crodua <jrcrodua@gmail.com>
2026-03-06 15:05:01 +0800

    fix(vite): remove html plugin entirely

--- dental-clinic/vite.config.js
+++ dental-clinic/vite.config.js
@@ -1,20 +1,3 @@
-import { defineConfig, loadEnv } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    // Load env file based on `mode` in the current working directory.
-    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-    const env = loadEnv(mode, process.cwd(), '');
-
-    return {
-        plugins: [
-            createHtmlPlugin({
-                inject: {
-                    data: {
-                        VITE_THEME: process.env.VITE_THEME || env.VITE_THEME || 'gold'
-                    }
-                }
-            })
-        ]
-    };
-});
+export default defineConfig({});
