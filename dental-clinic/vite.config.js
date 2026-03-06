a803d2752750fb5601f49355dab00eb87dfd96eb
Jun Rhy Crodua <jrcrodua@gmail.com>
2026-03-06 14:41:22 +0800

    add .env

---
+++ dental-clinic/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+export default defineConfig(({ mode }) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            createHtmlPlugin({
+                inject: {
+                    data: {
+                        VITE_THEME: env.VITE_THEME || 'gold'
+                    }
+                }
+            })
+        ]
+    };
+});
